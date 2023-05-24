5f0c7e01086beb7979b202c2f1bf0a43e9dad3c3
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2023-05-24 23:13:54 +0900

    send stderror to tee

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd `dirname $0`
 DATE=`date +%Y-%m-%d-%H-%M-%S`
-screen -d -S atcoder-discord-bot -m bash -c "deno task start | tee ./log/${DATE}.log"
+screen -d -S atcoder-discord-bot -m bash -c "deno task start 2>&1 | tee ./log/${DATE}.log"
 
