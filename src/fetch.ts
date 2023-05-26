1a6fa5421ba5bf14b12413756d3543b8433dcf0c
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2023-05-26 19:48:55 +0900

    change cache life to 30m

--- src/fetch.ts
+++ src/fetch.ts
@@ -4,13 +4,13 @@ const cache = new Map<string, {last_updated: number, response: string}>()
 
 /**
  * 指定されたURLのコンテンツを取得し、キャッシュします
- * キャッシュの有効期限は1時間です
+ * キャッシュの有効期限は30分です
  * @param url : 取得するURL
  * @returns 取得したコンテンツ(文字列)
  */
 export const cache_fetch = async (url: string): Promise<string> => {
   if(cache.has(url)) {
-    if(cache.get(url)!.last_updated + 1000 * 60 * 60 > DateTime.now().localData) {
+    if(cache.get(url)!.last_updated + 1000 * 60 * 30 > DateTime.now().localData) {
       return cache.get(url)!.response;
     }
   }
